c72c17f774936dbe622626cd9fa227e506e8bb67
shiro <9waiphyomg@gmail.com>
2024-01-20 13:03:03 +0630

    first commit

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>This is from invoices folder</div>
+  )
+}
+
+export default page
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Testing nesting routing.This is from dashboard</div>
+  )
+}
+
+export default page
